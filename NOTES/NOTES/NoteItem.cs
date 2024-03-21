7e13d7ba1390c1956b495f714c89ebd80c8ba8d0
Julia1067 <ugapon167@gmail.com>
2024-03-21 02:22:40 +0200

    Creating Note Item

---
+++ NOTES/NOTES/NoteItem.cs
@@ -0,0 +1,13 @@
+﻿namespace NOTES
+{
+    public class NoteItem
+    {
+        public Guid Id = Guid.NewGuid();
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
